4aafd8e2469fbe61aa4bac6a3fda4e0f31e80aef
Naveen Himthani <naveen@ices.utexas.edu>
2017-06-28 19:15:54 -0500

    smooth patient images using the matlab imgaussfilt3 function for .nii/.nc files

---
+++ smooth_brain.m
@@ -0,0 +1,22 @@
+function [ ] = smooth_brain( filenm, sigma )
+% filenm - path to .nii/.nc file
+% sigma - gaussian filter smoothing kernel standard deviation
+% output - new .nii/.nc file with smoothed data
+
+if filenm(end-3:end)=='.nii'
+    oldimage = load_nii(filenm);
+    newimage = imgaussfilt3(oldimage.img,sigma);
+    newimage = make_nii(newimage);
+    save_nii(newimage, ['smooth_',filenm]);
+
+elseif filenm(end-2:end)=='.nc'
+    oldimage = ncread(filenm, 'data');
+    newimage = imgaussfilt3(oldimage,sigma);
+    GenerateNCfile(['smooth_',filenm(1:end-3)], newimage);
+else
+    fprintf('please give either .nii or .nc file. exiting!');
+    return;
+end
+end
+
+

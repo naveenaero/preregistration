b60fb72565f8113a459a3dd5bdefcf88a5767f4b
Naveen Himthani <naveen@ices.utexas.edu>
2017-07-04 14:32:26 -0500

    multiplies and rescales, 2 probablity maps

---
+++ multiply_probs.m
@@ -0,0 +1,14 @@
+function [ ] = multiply_probs(filenm1, filenm2, newfilenm)
+
+data1 = ncread(filenm1,'data');
+data2 = ncread(filenm2,'data');
+%multiply
+newdata = data1.*data2;
+%rescale
+newdata(newdata>1)=1;
+newdata(newdata<0)=0;
+%create new .nc file
+GenerateNCfile(newdata, newfilenm);
+
+end
+
